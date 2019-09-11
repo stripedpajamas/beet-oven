ee7cc2a1555b5a063ea00413a468e8a3a257d15e
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-09-11 09:00:49 -0700

    Cleanup

--- site/script.js
+++ site/script.js
@@ -1,35 +1,19 @@
 const ARCHIVE_KEY = 'dat://92838f99f1081237eeab9c6cad1839995428e2a6464896d2a6bb83beb6587b2e'
 
-async function getRandomSong () {
-  const archive = await DatArchive.load(ARCHIVE_KEY)
-  const toc = JSON.parse(await archive.readFile('toc.json') || '[]')
-  return toc[Math.floor(Math.random() * toc.length)]
-}
-
 function getAudioFilePath ({ filename }) {
   return `${ARCHIVE_KEY}/${filename}`
 }
 
-async function playRandomSong (elements, firstTime) {
+async function playRandomSong (elements, toc, firstTime) {
   const { audio, title, composer, performer } = elements
-  console.log('Getting random song')
-  const song = await getRandomSong()
-  console.log('Got', song)
+  const song = toc[Math.floor(Math.random() * toc.length)]
+  if (!song) return
   audio.src = getAudioFilePath(song)
   if (!firstTime) audio.play()
 
   title.textContent = `${song.title}`
   composer.textContent = `${song.composer}`
   performer.textContent = `Performed by ${song.performer}`
-}
-
-function playpause () {
-  const audio = document.getElementById('audio')
-  if (audio.paused) {
-    audio.play()
-  } else {
-    audio.pause()
-  }
 }
 
 window.addEventListener('DOMContentLoaded', async () => {
@@ -39,11 +23,13 @@ window.addEventListener('DOMContentLoaded', async () => {
   const performer = document.getElementById('audio-info-performer')
   const elements = { audio, title, composer, performer }
 
-  playRandomSong(elements, true)
+  const archive = await DatArchive.load(ARCHIVE_KEY)
+  const toc = JSON.parse(await archive.readFile('toc.json') || '[]')
+
+  playRandomSong(elements, toc, true)
 
   audio.addEventListener('ended', () => {
-    console.log('Playback finished, getting another song')
-    playRandomSong(elements, false)
+    playRandomSong(elements, toc, false)
   })
 })
 
